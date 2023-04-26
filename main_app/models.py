b933f35eba9d23ad8dc266b3941c1ab3891a98de
2clubs <bennettdan85@gmail.com>
2023-04-26 13:44:08 -0400

    add book model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Book:
+    def __init__(self, title, author, publisher, year):
+        self.title = title
+        self.author = author
+        self.publisher = publisher
+        self.year = year
+        
+books = [
+    Book('Moby-Dick', 'Herman Melville', 'Penguin Books', 2003),
+    Book('Hard Times', 'Charles Dickens', 'Broadview Press', 2000),
+    Book('To Kill a Mockingbird', 'Harper Lee', 'J.B. Lippincott Company', 1960)
+]
