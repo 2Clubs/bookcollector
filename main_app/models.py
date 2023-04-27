795570f77d994ded34ab0196e9248f44c07c51fe
2clubs <bennettdan85@gmail.com>
2023-04-26 21:45:17 -0400

    adds detail page

--- main_app/models.py
+++ main_app/models.py
@@ -7,9 +7,13 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     publisher = models.CharField(max_length=100)
     isbn = ISBNField(clean_isbn=True, max_length=20)
+    
+    # override __str__ method
+    def __str__(self):
+        return self.title
         
-books = [
-    Book('Moby-Dick', 'Herman Melville', 'Penguin Books', 2003),
-    Book('Hard Times', 'Charles Dickens', 'Broadview Press', 2000),
-    Book('To Kill a Mockingbird', 'Harper Lee', 'J.B. Lippincott Company', 1960)
-]
+# books = [
+#     Book('Moby-Dick', 'Herman Melville', 'Penguin Books', 2003),
+#     Book('Hard Times', 'Charles Dickens', 'Broadview Press', 2000),
+#     Book('To Kill a Mockingbird', 'Harper Lee', 'J.B. Lippincott Company', 1960)
+# ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import books
+from .models import Book
 
 # Create your views here.
 def home(request):
@@ -9,5 +9,9 @@ def about(request):
     return render(request, 'about.html')
 
 def books_index(request):
-    # books = Book.objects.all()
+    books = Book.objects.all()
     return render(request, 'books/index.html', { 'books': books })
+
+def books_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request, 'books/detail.html', { 'book': book })
