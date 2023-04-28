39cdfb0c3581141d1f513120a12402aa29901cb2
2clubs <bennettdan85@gmail.com>
2023-04-28 01:30:15 -0400

    adds decorators and mixins

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,8 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Book, Genre
 from .forms import ReadingForm
 
@@ -13,16 +15,19 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def books_index(request):
     books = Book.objects.filter(user=request.user)
     return render(request, 'books/index.html', { 'books': books })
 
+@login_required
 def books_detail(request, book_id):
     book = Book.objects.get(id=book_id)
     reading_form = ReadingForm()
     genres_book_doesnt_have = Genre.objects.exclude(id__in = book.genre.all().values_list('id'))
     return render(request, 'books/detail.html', { 'book': book, 'reading_form': reading_form, 'genres': genres_book_doesnt_have })
 
+@login_required
 def add_reading(request, book_id):
     form = ReadingForm(request.POST)
     if form.is_valid():
@@ -30,7 +35,8 @@ def add_reading(request, book_id):
         new_reading.book_id = book_id
         new_reading.save()
     return redirect('detail', book_id=book_id)
-  
+
+@login_required  
 def assoc_genre(request, book_id, genre_id):
     Book.objects.get(id=book_id).genre.add(genre_id)
     return redirect('detail', book_id=book_id)
@@ -49,7 +55,7 @@ def signup(request):
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)  
 
-class BookCreate(CreateView):
+class BookCreate(LoginRequiredMixin, CreateView):
     model = Book
     fields = ('title', 'author', 'publisher', 'isbn')
     
@@ -57,28 +63,28 @@ class BookCreate(CreateView):
       form.instance.user = self.request.user
       return super().form_valid(form)
     
-class BookUpdate(UpdateView):
+class BookUpdate(LoginRequiredMixin, UpdateView):
     model = Book
     fields = '__all__'
     
-class BookDelete(DeleteView):
+class BookDelete(LoginRequiredMixin, DeleteView):
     model = Book
     success_url = '/books/'
     
-class GenreIndex(ListView):
+class GenreIndex(LoginRequiredMixin, ListView):
   model = Genre
 
-class GenreDetail(DetailView):
+class GenreDetail(LoginRequiredMixin, DetailView):
   model = Genre
   
-class GenreCreate(CreateView):
+class GenreCreate(LoginRequiredMixin, CreateView):
   model = Genre
   fields = '__all__'
 
-class GenreUpdate(UpdateView):
+class GenreUpdate(LoginRequiredMixin, UpdateView):
   model = Genre
   fields = '__all__'
 
-class GenreDelete(DeleteView):
+class GenreDelete(LoginRequiredMixin, DeleteView):
   model = Genre
   success_url = '/genre/'
